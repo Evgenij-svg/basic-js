1639b577e6ef442548f7527ae465b2477edac93e
Evgenij-svg <deshkevich.evgenij@gmail.com>
2022-10-16 22:15:15 +0300

    feat:common-character and sum digit

--- src/common-character-count.js
+++ src/common-character-count.js
@@ -11,11 +11,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For s1 = "aabcc" and s2 = "adcaa", the output should be 3
  * Strings have 3 common characters - 2 "a"s and 1 "c".
  */
-function getCommonCharacterCount(/* s1, s2 */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+
+function getCommonCharacterCount(s1, s2) {
+    const s1Arr = s1.split('');
+    const s2Arr = s2.split('');
+    let sum = 0;
+    while (true) {
+        s1Arr.forEach((elem, indx) => {
+            s2Arr.forEach((e, i) => {
+                if (elem == e) {
+                    sum++;
+                    s1Arr.splice(indx, 1);
+                    s2Arr.splice(i, 1);
+                }
+            });
+        });
+        let op = 1;
+        s1Arr.forEach((elem, indx) => {
+            s2Arr.forEach((e, i) => {
+                if (elem == e) {
+                    op--;
+                }
+            });
+        });
+        if ((op == 1)) {
+            break;
+        }
+    }
+    return sum;
 }
 
 module.exports = {
-  getCommonCharacterCount
+    getCommonCharacterCount,
 };
+
--- src/sum-digits.js
+++ src/sum-digits.js
@@ -12,11 +12,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getSumOfDigits(n) {
+    let StrNumber = n + '';
+    let sum = 0;
+    StrNumber = StrNumber.split('');
+    if (StrNumber.length > 1) {
+        StrNumber.forEach((element) => {
+            sum = Number(element) + sum;
+        });
+        if (sum > 9) {
+            getSumOfDigits(sum);
+        } else {
+            return sum;
+        }
+    } else {
+        sum = n;
+
+        return sum;
+    }
 }
 
 module.exports = {
-  getSumOfDigits
+    getSumOfDigits,
 };
+
