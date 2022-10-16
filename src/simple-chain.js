9a75123caf5f9f1c82a6b98d3a90aa6570ab10f4
Evgenij-svg <deshkevich.evgenij@gmail.com>
2022-10-16 20:20:16 +0300

    fix:task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -6,21 +6,26 @@ const { NotImplementedError } = require('../extensions/index.js');
  */
 
 const chainMaker = {
-  let str=[];
+  
   getLength() {
-
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
-  addLink(value) {
-    this.str.push(value);
+  addLink(/* value */) {
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   removeLink(/* position */) {
-
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   reverseChain() {
-
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   },
   finishChain() {
-    return this.str;
+    throw new NotImplementedError('Not implemented');
+    // remove line with error and write your code here
   }
 };
 
