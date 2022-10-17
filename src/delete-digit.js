9f7c04eeca31a8560502d858bf0e773a787f196a
Evgenij-svg <deshkevich.evgenij@gmail.com>
2022-10-17 22:32:11 +0300

    feat:tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,24 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
+function deleteDigit(n) {
+const ArrayNumber=(n+'').split('')
+const MaxNumber=0;
+
+ArrayNumber.forEach((elem, index)=>{
+  const BuferArray=ArrayNumber;
+  console.log(Number((BuferArray.slice(index,ArrayNumber.length).toString().replace(/,/g, ''))))
+  // if(MaxNumber<Number(BuferArray.splice(index+1,1).toString())){
+
+  // }
+})
+
+
   // remove line with error and write your code here
 }
 
 module.exports = {
   deleteDigit
 };
+
+deleteDigit(152);
--- src/simple-chain.js
+++ src/simple-chain.js
@@ -2,33 +2,58 @@ const { NotImplementedError } = require('../extensions/index.js');
 
 /**
  * Implement chainMaker object according to task description
- * 
+ *
  */
 
 const chainMaker = {
-  
-  getLength() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  reverseChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  },
-  finishChain() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
-  }
+    Chain: [],
+    getLength() {
+        return this.Chain.length;
+    },
+    addLink(value) {
+        this.Chain.push(`( ${value} )`);
+        return chainMaker;
+    },
+    removeLink(position) {
+        if (position < 1) {
+            return new Error(
+                'throws an Error with message "You can\'t remove incorrect link!" on trying to remove wrong link'
+            );
+         }else if (position > this.Chain.length) {
+            return new Error(
+                'throws an Error with message "You can\'t remove incorrect link!" on trying to remove wrong link'
+            );
+        } else if (!(typeof position == 'number')) {
+            return new Error(
+                'throws an Error with message "You can\'t remove incorrect link!" on trying to remove wrong link'
+            );
+        }
+        else{
+            this.Chain.splice(position - 1, 1);
+        }
+        
+        return chainMaker;
+    },
+    reverseChain() {
+        this.Chain.reverse();
+        return chainMaker;
+    },
+    finishChain() {
+        return this.Chain.toString().replace(/,/g, '~~');
+    },
 };
 
 module.exports = {
-  chainMaker
+    chainMaker,
 };
+console.log(
+    chainMaker
+        .addLink(function () {})
+        .addLink('2nd')
+        .addLink('3rd')
+        .removeLink(2)
+        .reverseChain()
+        .finishChain()
+);
+//'( 3rd )~~( function () { } )'
+// ( 3rd )~~( function () { } )
